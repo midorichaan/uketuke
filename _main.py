a06da9447e1b63c927d2eeb348b4838f0e411cde
midorichaan <>
2022-06-22 15:33:01 +0900

    save

---
+++ _main.py
@@ -0,0 +1,105 @@
+import aiohttp
+import asyncio
+import os
+import sys
+import tkinter
+import tkinter.font as font
+from logging import basicConfig, getLogger, INFO
+from dotenv import load_dotenv
+
+from lib import database
+
+#init environment
+load_dotenv()
+basicConfig(
+    level=INFO, 
+    format="%(asctime)s - %(name)s - [%(levelname)s]: %(message)s"
+)
+
+#vars
+loop = asyncio.get_event_loop()
+logger = getLogger("__main__")
+session = aiohttp.ClientSession(loop=loop)
+db = database.Database(
+    host=os.environ["DB_HOST"],
+    port=int(os.environ["DB_PORT"]),
+    user=os.environ["DB_USER"],
+    password=os.environ["DB_PASSWD"],
+    db=os.environ["DB_DATABASE"]
+)
+root = tkinter.Tk()
+font = font.Font(root, family="無心", size=20)
+modules = {}
+
+#clear_text
+def clear_text():
+    modules["text_name"] = ""
+
+#programs
+def setup_root(manage: bool=False):
+    global root, modules
+    logger.info("SETUP: setting up...")
+
+    if manage:
+        logger.info("SETUP: logged in as root")
+        root.title(u"受付管理システム (管理用)")
+    else:
+        logger.info("SETUP: logged in as user")
+        root.title(u"受付管理システム (一般用)")
+
+    tb = tkinter.Label(root,text="名前")
+    txt = tkinter.Entry(root)
+    ok = tkinter.Button(root, text="完了", font=font, command=lambda: None)
+    cl = tkinter.Button(root, text="入力クリア", font=font, command=clear_text)
+    exit = tkinter.Button(root,text="終了", font=font, command=root.destroy)
+
+    #place entry
+    tb.pack()
+    txt.pack()
+    ok.pack(side=tkinter.LEFT)
+    cl.pack(side=tkinter.LEFT)
+    exit.pack(side=tkinter.LEFT)
+
+    #register global vars
+    modules["title"] = tb
+    modules["text_name"] = txt
+    modules["ok_button"] = ok
+    modules["clear_button"] = cl
+    modules["exit_button"] = exit
+
+    root.geometry("600x400")
+    root.resizable(False, False)
+
+#init_database
+async def init_database():
+    logger.info("SETUP: database init")
+    await db.execute(
+        "CREATE TABLE IF NOT EXISTS uketuke(user_id INTEGER, name TEXT, point BIGINT)",
+    )
+
+#handle_args
+def handle_args(args):
+    if len(args) == 2:
+        if args[1] == "-staff":
+            return True
+        elif args[1] == "-user":
+            return False
+    else:
+        logger.error("INIT: setup failed (invalid args)")
+        return
+    
+if __name__ == "__main__":
+    args = sys.argv
+    ret = handle_args(args)
+    if ret == None:
+        logger.error("RUN: error while handling args. exit")
+    else:
+        #run
+        try:
+            setup_root(ret)
+            root.mainloop()
+        except Exception as exc:
+            logger.error(f"RUN: error while running: {exc}")
+        finally:
+            root.quit()
+            loop.run_until_complete(session.close())
--- main.py
+++ main.py
@@ -1,25 +1,25 @@
-import aiohttp
+# -*- coding: utf-8 -*-
 import asyncio
 import os
 import sys
-import tkinter
+import tkinter as tk
 import tkinter.font as font
-from logging import basicConfig, getLogger, INFO
+
 from dotenv import load_dotenv
-
 from lib import database
+from logging import basicConfig, getLogger, INFO
 
-#init environment
+#load .env
 load_dotenv()
+
+#logger
 basicConfig(
-    level=INFO, 
-    format="%(asctime)s - %(name)s - [%(levelname)s]: %(message)s"
+	level=INFO,
+	format="%(asctime)s - %(name)s - [%(levelname)s]: %(message)s"
 )
 
-#vars
-loop = asyncio.get_event_loop()
-logger = getLogger("__main__")
-session = aiohttp.ClientSession(loop=loop)
+#global vars
+logger = getLogger("manage-system")
 db = database.Database(
     host=os.environ["DB_HOST"],
     port=int(os.environ["DB_PORT"]),
@@ -27,67 +27,130 @@ db = database.Database(
     password=os.environ["DB_PASSWD"],
     db=os.environ["DB_DATABASE"]
 )
-root = tkinter.Tk()
-font = font.Font(root, family="無心", size=20)
 
-#programs
-def setup_root(manage: bool=False):
-    global root
-    logger.info("SETUP: setting up...")
+class Application(tk.Tk):
 
-    if manage:
-        logger.info("SETUP: logged in as root")
-        root.title(u"受付管理システム (管理用)")
-    else:
-        logger.info("SETUP: logged in as user")
-        root.title(u"受付管理システム (一般用)")
+    def __init__(self, *, managed: bool=False):
+        super().__init__()
 
-    tb = tkinter.Label(root,text="名前")
-    txt = tkinter.Entry(root)
-    ok = tkinter.Button(root, text="完了", font=font, command=lambda: None)
-    cl = tkinter.Button(root, text="入力クリア", font=font, command=lambda: txt.set(""))
-    exit = tkinter.Button(root,text="終了", font=font, command=root.destroy)
+        self.entries = {}
+        self._managed = managed
+        self._id = None
+        self._font = font.Font(self, family="無心", size=20)
+        self._init()
+        asyncio.gather(self._init_database())
 
-    #place entry
-    tb.pack()
-    txt.pack()
-    ok.pack()
-    cl.pack()
-    exit.pack()
+    #_init_database
+    async def _init_database(self):
+    	await db.execute(
+    		"CREATE TABLE IF NOT EXISTS uketuke(id BIGINT, name TEXT)"
+    	)
 
-    root.geometry("400x200")
-    root.resizable(False, False)
+    #_get_id_from_db
+    async def _get_id_from_db(self):
+    	query = await db.fetchall("SELECT * FROM uketuke")
+    	self._id = len(query) + 1
 
-#init_database
-async def init_database():
-    logger.info("SETUP: database init")
-    await db.execute(
-        "CREATE TABLE IF NOT EXISTS uketuke(user_id INTEGER, name TEXT, point BIGINT)",
-    )
+    #_get_id
+    def _get_id(self):
+    	asyncio.gather(self._get_id_from_db())
+    	return self._id
 
-#handle_args
-def handle_args(args):
+    #_submit_data
+    def _submit_data(self):
+        logger.info("APP: data submitted")
+        data = {
+            "name": self.entries["name_entry"].get(),
+            "id": self._get_id()
+        }
+        logger.info(f"APP: submitted data - {data}")
+        self._delete_text()
+
+        if not data["name"]:
+            logger.warning("APP: name is a required argument that is missing")
+       	    return
+       	else:
+       		query = db.execute(
+       			"INSERT INTO uketuke VALUES(%s, %s)",
+       			(data["id"], data["name"])
+       		)
+       		asyncio.gather(query)
+
+    #_delete_text
+    def _delete_text(self):
+    	logger.info("APP: text 'name' cleared")
+    	self.entries["name_entry"].delete(0, tk.END)
+
+    #_init
+    def _init(self):
+        if self._managed:
+            logger.info("APP: application run as staff")
+            self.title("受付管理システム (管理用)")
+        else:
+            logger.info("APP: application run as user")
+            self.title("受付システム (一般用)")
+
+        self.geometry("600x400")
+        self.resizable(False, False)
+
+        #label
+        name_label = tk.Label(self, text="名前", font=self._font)
+        #text box
+        name_entry = tk.Entry()
+        name_entry.configure(state="normal", width=30)
+        #buttons
+        submit = tk.Button(text="完了", width=20, command=self._submit_data)
+        cancel = tk.Button(text="キャンセル", width=20, command=self._delete_text)
+        exit = tk.Button(text="終了", width=20, command=lambda: self.quit())
+
+        #add data
+        self.entries["name_label"] = name_label
+        self.entries["name_entry"] = name_entry
+        self.entries["submit_button"] = submit
+        self.entries["cancel_button"] = cancel
+        self.entries["exit_button"] = exit
+
+        #place items
+        """
+        for k, v in self.entries.items():
+        	if "entry" in str(k):
+        		v.pack()
+        	elif "label" in str(k):
+        		v.pack()
+        	elif "button" in str(k):
+        		v.grid()
+        """
+        self.entries["name_label"].pack()
+        self.entries["name_entry"].pack()
+        self.entries["submit_button"].pack()
+        self.entries["cancel_button"].pack()
+        self.entries["exit_button"].pack()
+
+#handle_argunent
+def handle_argument(args):
     if len(args) == 2:
         if args[1] == "-staff":
             return True
         elif args[1] == "-user":
             return False
+        else:
+            return
     else:
-        logger.error("INIT: setup failed (invalid args)")
-        return
-    
+	    return
+
 if __name__ == "__main__":
     args = sys.argv
-    ret = handle_args(args)
-    if ret == None:
-        logger.error("RUN: error while handling args. exit")
-    else:
-        #run
+    val = handle_argument(args)
+    app = None
+
+    if not val == None:
         try:
-            setup_root(ret)
-            root.mainloop()
+            app = Application(managed=val)
+            app.mainloop()
+            logger.info("RUN: successfully runned application")
         except Exception as exc:
-            logger.error(f"RUN: error while running: {exc}")
-        finally:
-            root.quit()
-            loop.run_until_complete(session.close())
+            logger.error(f"RUN: {exc}")
+            app.quit()
+            logger.info("RUN: application exited")
+    else:
+        logger.error("RUN: invalid argument")
